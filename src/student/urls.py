112711f498aa38fe8f4253252f73768ff5e24311
anburps <anbalaganrps@gmail.com>
2024-09-09 20:54:19 +0530

    list,update and delete

--- src/student/urls.py
+++ src/student/urls.py
@@ -5,5 +5,8 @@ from .views import *
 
 urlpatterns = [
     path('student_detail/',StudentDetailCreate.as_view()),
+    path('student_detail/<int:pk>/',StudentDetailUpdate.as_view()),
+    path('student_detail/<int:pk>/delete/',StudentDetailDelete.as_view()),
+    path('student_detail_list/',StudentDetailList.as_view()),
     
 ]
--- src/student/views.py
+++ src/student/views.py
@@ -31,3 +31,42 @@ class StudentDetailCreate(APIView):
                 'error': serializer.errors,
             }
             return Response(content_data)
+
+class StudentDetailList(APIView):
+    serializer_class = StudentSerializers.StudentDetailSerializer
+    def get(self, request, *args, **kwargs):
+        get_data_qset = StudentSerializers.StudentDetailSerializer
+        serializer = StudentSerializers.StudentDetailSerializer(get_data_qset, many=True)
+        content_data = {
+            'provided_by': "SMS API services",
+            'success': True,
+            'status': 200,
+            'data': serializer.data,
+        }
+        return Response(content_data)
+
+class StudentDetailUpdate(APIView):
+    serializer_class = StudentSerializers.StudentDetailSerializer
+    def get(self, request, *args, **kwargs):
+        get_data_qset = StudentSerializers.StudentDetailSerializer
+        serializer = StudentSerializers.StudentDetailSerializer(get_data_qset, many=True)
+        content_data = {
+            'provided_by': "SMS API services",
+            'success': True,
+            'status': 200,
+            'data': serializer.data,
+        }
+        return Response(content_data)
+
+class StudentDetailDelete(APIView):
+    serializer_class = StudentSerializers.StudentDetailSerializer
+    def get(self, request, *args, **kwargs):
+        get_data_qset = StudentSerializers.StudentDetailSerializer
+        serializer = StudentSerializers.StudentDetailSerializer(get_data_qset, many=True)
+        content_data = {
+            'provided_by': "SMS API services",
+            'success': True,
+            'status': 200,
+            'data': serializer.data,
+        }
+        return Response(content_data)
