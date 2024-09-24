0099c0d3518846f58ca03f2c1dcfaab0dc379443
anburps <anbalaganrps@gmail.com>
2024-09-24 19:45:17 +0530

    course detail full patical update

--- src/student/views.py
+++ src/student/views.py
@@ -193,6 +193,28 @@ class CourseDetailView(generics.RetrieveUpdateDestroyAPIView):
                 'error': serializer.errors,
             }
             return Response(content_data, status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request, *args, **kwargs):
+        course = self.get_object() 
+        serializer = self.get_serializer(course, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            content_data = {
+                'provided_by': "SMS API services",
+                'success': True,
+                'status': 200,
+                'data': serializer.data,
+            }
+            return Response(content_data, status=status.HTTP_200_OK)
+        else:
+            content_data = {
+                'provided_by': "SMS API services",
+                'success': False,
+                'status': 400,
+                'error': serializer.errors,
+            }
+            return Response(content_data, status=status.HTTP_400_BAD_REQUEST)
 
 class EnrollmentCreateView(GenericAPIView):
     serializer_class = StudentSerializers.EnrollmentSerializer
