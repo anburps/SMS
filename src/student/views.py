ec1a4a168fdcb0a199f87f679787a1c712b135c9
anburps <anbalaganrps@gmail.com>
2024-09-24 19:41:42 +0530

    course list and create

--- src/student/views.py
+++ src/student/views.py
@@ -118,7 +118,7 @@ class StudentDetailView(generics.RetrieveUpdateDestroyAPIView):
         }
         return Response(content_data, status=status.HTTP_204_NO_CONTENT)
 
-class CourseCreateView(GenericAPIView):
+class CourseListCreateView(generic.ListCreateAPIView):
     serializer_class = StudentSerializers.CourseSerializer
 
     def post(self, request, *args, **kwargs):
@@ -140,8 +140,29 @@ class CourseCreateView(GenericAPIView):
                 'error': serializer.errors,
             }
             return Response(content_data, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        queryset = Course.objects.all()
 
+        if queryset.exists():
+            serializer = self.get_serializer(queryset, many=True)
+            content_data = {
+                'provided_by': "SMS API services",
+                'success': True,
+                'status': 200,
+                'data': serializer.data,
+                'count': queryset.count(),
+            }
+            return Response(content_data, status=status.HTTP_200_OK)
+        else:
+            content_data = {
+                'provided_by': "SMS API services",
+                'success': False,
+                'status': 400,
+                'error': "No data found",
+            }
+            return Response(content_data, status=status.HTTP_400_BAD_REQUEST)
 
+            
 class EnrollmentCreateView(GenericAPIView):
     serializer_class = StudentSerializers.EnrollmentSerializer
 
