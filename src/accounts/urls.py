961961803df666ff017f016d628c22cb78628bfd
anburps <anbalaganrps@gmail.com>
2024-09-27 21:45:05 +0530

    sms app urls

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UserRegistrationView, UserLoginView
+from .views import RegisterView, LoginView, LogoutView, ProtectedView
 
 urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='register'),
-    path('login/', UserLoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('protected/', ProtectedView.as_view(), name='protected'),
 ]
