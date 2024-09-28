72ad97ac4ee5c3bc5937ac1b91a06a6458e58489
anburps <anbalaganrps@gmail.com>
2024-09-28 22:04:56 +0530

    token generated

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,43 +1,50 @@
 from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.authentication import BasicAuthentication
-from django.contrib.auth import login,logout
-from .models import * 
-from .serializers import *
-from rest_framework.permissions import IsAuthenticated  
+from rest_framework.authentication import BasicAuthentication, TokenAuthentication
+from django.contrib.auth import login, authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
+from .models import User
+from .serializers import LoginSerializer, UserSerializer, RegisterSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [permissions.AllowAny]
 
-class LoginView(APIView):
+class LoginView(ObtainAuthToken):
     serializer_class = LoginSerializer
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]  
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            user = serializer.validated_data['user']
-            print("user", user)
-            login(request, user)
-            return Response({"message": "Login successful."}, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
 
+        token, created = Token.objects.get_or_create(user=user)
+
+        login(request, user)
+
+        return Response({
+            'token': token.key,
+            'user_id': user.id,
+            'email': user.email,
+            'username': user.username
+        })
 
 class LogoutView(APIView):
-    permission_classes = [IsAuthenticated]  
-    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication, BasicAuthentication]
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
+        request.user.auth_token.delete()
         logout(request)
         return Response({"message": "Logout successful."}, status=status.HTTP_200_OK)
 
 class ProtectedView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
-    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication, BasicAuthentication]
 
     def get(self, request):
         user = request.user
