b2f17daeed6cd4f06d808fbea41e509164a002ab
anburps <anbalaganrps@gmail.com>
2024-09-28 21:39:02 +0530

    login success

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -2,26 +2,30 @@ from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authentication import BasicAuthentication
-from django.contrib.auth import login, logout
-from .models import *
+from django.contrib.auth import login
+from .models import * 
 from .serializers import *
+from rest_framework.permissions import IsAuthenticated  
+
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [permissions.AllowAny]
 
 class LoginView(APIView):
-    permission_classes = [permissions.AllowAny]
     serializer_class = LoginSerializer
     authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]  
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             user = serializer.validated_data['user']
+            print("user", user)
             login(request, user)
             return Response({"message": "Login successful."}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class LogoutView(APIView):
     permission_classes = [permissions.IsAuthenticated]
