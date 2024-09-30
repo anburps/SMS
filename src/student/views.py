f8a8419c0b157a343091e417664f2689f3b1d55f
anburps <anbalaganrps@gmail.com>
2024-09-30 20:54:59 +0530

    student detail view like put ,patch and delete

--- src/student/views.py
+++ src/student/views.py
@@ -10,7 +10,7 @@ from rest_framework.authentication import BasicAuthentication,TokenAuthenticatio
 from rest_framework.permissions import IsAuthenticated,AllowAny
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import PageNumberPagination
-# pagination.py
+
 class CustomPageNumberPagination(PageNumberPagination):
     page_size = 2  
     page_size_query_param = 'page_size'  
@@ -89,12 +89,26 @@ class StudentListCreateView(generics.ListCreateAPIView):
 class StudentDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Student.objects.all()
     serializer_class =  StudentSerializers.StudentDetailSerializer
-
+    authentication_classes = [BasicAuthentication,TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     def get_object(self):
         try:
             return Student.objects.get(id=self.kwargs['id'])
         except Student.DoesNotExist:
             raise Http404("Student not found.")
+    
+    def get(self, request, *args, **kwargs):
+        student = self.get_object()
+        serializer = self.get_serializer(student)
+        content_data = {
+            'provided_by': "SMS API services",
+            'success': True,
+            'status': 200,
+            'data': serializer.data
+
+        }
+
+        return Response(content_data, status=status.HTTP_200_OK)
 
     def put(self, request, *args, **kwargs):
         student = self.get_object()  
