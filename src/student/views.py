b0140a17b72542a27cc4280884155eea73b44321
anburps <anbalaganrps@gmail.com>
2024-10-02 19:50:21 +0530

    Entrollment delete function

--- src/student/views.py
+++ src/student/views.py
@@ -391,6 +391,18 @@ class EntrollmentDetailView(generics.RetrieveUpdateDestroyAPIView):
                 'error': serializer.errors,
             }
             return Response(content_data, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, *args, **kwargs):
+        enrollment=self.get_object()
+        enrollment.delete()
+        content_data = {
+            'provided_by': "SMS API services",
+            'success': True,
+            'status': 204,
+            'message': "Enrollment successfully deleted."
+        }
+        return Response(content_data, status=status.HTTP_204_NO_CONTENT)
+
+        
 class GradeCreateView(GenericAPIView):
     serializer_class = StudentSerializers.GradeSerializer
 
