c1f7c843c8351e557a48efe107edd620c3d0be21
anburps <anbalaganrps@gmail.com>
2024-10-02 19:57:19 +0530

    post and get method

--- src/student/views.py
+++ src/student/views.py
@@ -439,6 +439,25 @@ class GradeCreateView(GenericAPIView):
                 'error': serializer.errors,
             }
             return Response(content_data, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        if queryset.exists():
+            serializer = self.get_serializer(queryset, many=True)
+            content_data = {
+                'provided_by': "SMS API services",
+                'success': True,
+                'status': 200,
+                'data': serializer.data,
+                'count': queryset.count(),
+            }
+            return Response(content_data, status=status.HTTP_200_OK)
+        else:
+            content_data = {
+                'provided_by': "SMS API services",
+                'success': False,
+                'status': 400,
+                'error': "No data found",
+            }
+            return Response(content_data, status=status.HTTP_400_BAD_REQUEST)
 
 
 class AttendanceCreateView(GenericAPIView):
