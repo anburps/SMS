3496ede9e45d75010fdd0ce73e54ec02f04bf88f
anburps <anbalaganrps@gmail.com>
2024-10-03 21:41:12 +0530

    account models

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -6,27 +6,27 @@ class MyUserManager(UserManager):
     def create_user(self, email, password=None, **kwargs):
         if not email:
             raise ValueError(_('The Email field must be set'))
-        email = self.normalize_email(email)
-        user = self.model(email=email, **kwargs)
+        email           = self.normalize_email(email)
+        user            = self.model(email=email, **kwargs)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
     def create_superuser(self, email, password=None, **kwargs):
-        user = self.create_user(email=email, password=password, **kwargs)
-        user.is_staff = True
-        user.is_superuser = True
+        user                = self.create_user(email=email, password=password, **kwargs)
+        user.is_staff       = True
+        user.is_superuser   = True
         user.save(using=self._db)
         return usera
 
 class User(AbstractBaseUser):
-    email = models.EmailField(_('email address'), unique=True)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    email       = models.EmailField(_('email address'), unique=True)
+    is_staff    = models.BooleanField(default=False)
+    is_active   = models.BooleanField(default=True)
 
     objects = MyUserManager()
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD  = 'email'
     REQUIRED_FIELDS = []
 
     class Meta:
