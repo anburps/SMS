8a6f9e63c9488090f8b71ac95c1bfd833fa132c6
anburps <anbalaganrps@gmail.com>
2024-10-04 20:15:54 +0530

    grade delete method

--- src/student/views.py
+++ src/student/views.py
@@ -507,7 +507,17 @@ class GradeDetailView(GenericAPIView):
                 'error': serializer.errors,
             }
             return Response(content_data, status=status.HTTP_400_BAD_REQUEST)
-            
+    def delete(self, request, *args, **kwargs):
+        grade = self.get_object()
+        grade.delete()
+        content_data = {
+            'provided_by': "SMS API services",
+            'success': True,
+            'status': 204,
+            'message': "Grade successfully deleted."
+        }
+        return Response(content_data, status=status.HTTP_204_NO_CONTENT)
+        
 class AttendanceCreateView(GenericAPIView):
     serializer_class = StudentSerializers.AttendanceSerializer
 
