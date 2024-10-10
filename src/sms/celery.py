b2782f46beb0a635cf7f25a25b633433af9f9d4f
anburps <anbalaganrps@gmail.com>
2024-10-10 20:20:57 +0530

    celery file added

--- src/sms/celery.py
+++ src/sms/celery.py
@@ -3,9 +3,9 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sms.settings')
 
-app = Celery('your_project_name')
+app = Celery('sms')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
