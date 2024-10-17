091d4db2fde3510617b55449ae05e2edf7ce9537
anburps <anbalaganrps@gmail.com>
2024-10-17 20:48:21 +0530

    attendance list

--- src/student/views.py
+++ src/student/views.py
@@ -554,3 +554,29 @@ class AttendanceCreateView(GenericAPIView):
                 'error': serializer.errors,
             }
             return Response(content_data, status=status.HTTP_400_BAD_REQUEST)
+class AttendanceListView(generic.ListAPIView):
+    serializer_class = StudentSerializers.AttendanceSerializer
+    queryset = Attendance.objects.all()
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, *args, **kwargs):
+        if queryset.exists():
+            serializer = self.get_serializer(queryset, many=True)
+            content_data = {
+                'provided_by': "SMS API services",
+                'success': True,
+                'status': 200,
+                'data': serializer.data,
+                'count': queryset.count(),
+            }
+            return Response(content_data, status=status.HTTP_200_OK)
+        else:
+            content_data = {
+                'provided_by': "SMS API services",
+                'success': False,
+                'status': 400,
+                'error': "No data found",
+            }
+            return Response(content_data, status=status.HTTP_400_BAD_REQUEST)
+
