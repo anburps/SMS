60a4bcd4dc7cf5807f4090aaa1585cd41364ab3a
anburps <anbalaganrps@gmail.com>
2024-10-19 20:52:39 +0530

    pagination added

--- src/student/views.py
+++ src/student/views.py
@@ -471,17 +471,17 @@ class GradeListView(generic.ListAPIView):
     pagination_class = pagination.PageNumberPagination
 
     def get(self, request, *args, **kwargs):
-        if queryset.exists():
-            
-            serializer = self.get_serializer(queryset, many=True)
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
             content_data = {
                 'provided_by': "SMS API services",
                 'success': True,
                 'status': 200,
                 'data': serializer.data,
-                'count': queryset.count(),
             }
-            return Response(content_data, status=status.HTTP_200_OK)
+            return self.get_paginated_response(content_data)
         else:
             content_data = {
                 'provided_by': "SMS API services",
@@ -490,6 +490,8 @@ class GradeListView(generic.ListAPIView):
                 'error': "No data found",
             }
             return Response(content_data, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 class GradeDetailView(generic.RetrieveUpdateDestroyAPIView):
     serializer_class = StudentSerializers.GradeSerializer
