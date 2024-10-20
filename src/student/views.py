9d76b89de8f7656901d80b85a502db3fee126ebb
anburps <anbalaganrps@gmail.com>
2024-10-20 18:34:56 +0530

    pagination apply

--- src/student/views.py
+++ src/student/views.py
@@ -673,23 +673,21 @@ class TeacherListView(generic.ListAPIView):
     authentication_classes = [BasicAuthentication, TokenAuthentication]
     permission_classes = [IsAuthenticated]
     filter_backends = [SearchFilter]
+    pagination_class = pagination.PageNumberPagination
+
 
     def get(self, request, *args, **kwargs):
-        cache_data = cache.get("attendance")
-        if cache_data:
-            return Response(cache_data)
-
-        if queryset.exists():
-            serializer = self.get_serializer(queryset, many=True)
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
             content_data = {
                 'provided_by': "SMS API services",
                 'success': True,
                 'status': 200,
                 'data': serializer.data,
-                'count': queryset.count(),
             }
-            cache.set("attendance", content_data,timeout=300)
-            return Response(content_data, status=status.HTTP_200_OK)
+            return self.get_paginated_response(content_data)
         else:
             content_data = {
                 'provided_by': "SMS API services",
@@ -698,4 +696,3 @@ class TeacherListView(generic.ListAPIView):
                 'error': "No data found",
             }
             return Response(content_data, status=status.HTTP_400_BAD_REQUEST)
-    
