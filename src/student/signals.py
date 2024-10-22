d27c6950cc2714851b39845779b7698871cca5a9
anburps <anbalaganrps@gmail.com>
2024-10-22 19:15:54 +0530

    employee create after employee is and hire date mail send

--- src/student/signals.py
+++ src/student/signals.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 @receiver(post_save, sender=Student)
 def send_email_after_student_creation(sender, instance, created, **kwargs):
     if created:
-        password = instance.date_of_birth.strftime('%Y-%m-%d')  # Format as a string
+        password = instance.date_of_birth.strftime('%Y-%m-%d') 
 
         user = User.objects.create_user(
             username=instance.email,  
@@ -22,4 +22,12 @@ def send_email_after_student_creation(sender, instance, created, **kwargs):
         recipient_list = [instance.email] 
         send_mail(subject, message, from_email, recipient_list)
 
-@
+@receiver(post_save,sender=Teacher)
+def send_email_employee_creation(sender,instance,created, **kwargs):
+    if created:
+        subject = "Welcome to Our Service"
+        message = f"Dear {instance.first_name}{instance.last_name},\n\nWelcome to our service! Your details have been successfully registered.\n\n employee_id: {instance.employee_id}\hire_date: {instance.hire_date}\n\nThank you for choosing our service." 
+        from_email = settings.DEFAULT_FROM_EMAIL 
+        recipient_list = [instance.email] 
+        send_mail(subject, message, from_email, recipient_list)
+        
