92e7822032d9eb1ec365ce6e6e877b0db31d0fb3
anburps <anbalaganrps@gmail.com>
2024-10-29 19:38:47 +0530

    authentication and permission added

--- src/student/views.py
+++ src/student/views.py
@@ -759,8 +759,9 @@ class AssignmentCreateView(generic.CreateAPIView):
 class AssignmentListView(generic.ListAPIView):
     serializer_class = StudentSerializers.AssignmentSerializer
     queryset = Assignment.objects.all()
+    authentication_classes = [BasicAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     pagination_class = pagination.PageNumberPagination
-
     def get(self,request,*args,**kwargs):
         queryset = self.filter_queryset(self.get_queryset())
         page = self.paginate_queryset(queryset)
