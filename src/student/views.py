bac710cc3b3ccac8adad2ae0aa682d8a0ea54d18
anburps <anbalaganrps@gmail.com>
2024-10-29 19:34:01 +0530

    Assignment list show

--- src/student/views.py
+++ src/student/views.py
@@ -755,3 +755,28 @@ class AssignmentCreateView(generic.CreateAPIView):
                 'error': serializer.errors,
             }
             return Response(content_data, status=status.HTTP_400_BAD_REQUEST)
+
+class AssignmentListView(generic.ListAPIView):
+    serializer_class = StudentSerializers.AssignmentSerializer
+    queryset = Assignment.objects.all()
+
+    def get(self,request,*args,**kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            content_data = {
+                'provided_by': "SMS API services",
+                'success': True,
+                'status': 200,
+                'data': serializer.data,
+            }
+            return self.get_paginated_response(content_data)
+        else:
+            content_data = {
+                'provided_by': "SMS API services",
+                'success': False,
+                'status': 400,
+                'error': "No data found",
+            }
+            return Response(content_data, status=status.HTTP_400_BAD_REQUEST)
