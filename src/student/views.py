6adddf4862a0b0f022a5dde5db4f4b266cf5e38d
anburps <anbalaganrps@gmail.com>
2024-10-30 20:37:20 +0530

    attenance data update function added

--- src/student/views.py
+++ src/student/views.py
@@ -766,6 +766,7 @@ class AssignmentListView(generic.ListAPIView):
     search_fields = ['title']
     ordering_fields = ['title']
     pagination_class = pagination.PageNumberPagination
+
     def get(self,request,*args,**kwargs):
         queryset = self.filter_queryset(self.get_queryset())
         page = self.paginate_queryset(queryset)
@@ -784,5 +785,32 @@ class AssignmentListView(generic.ListAPIView):
                 'success': False,
                 'status': 400,
                 'error': "No data found",
+            }
+            return Response(content_data, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AttenanceDetialsView(generics.RetrieveAPIView):
+    serializer_class = StudentSerializers.AttendanceSerializer
+    queryset = Attendance.objects.all()
+   
+
+    def patch(self,request, *args, **kwargs):
+        attendance = self.get_object()
+        serializer = self.get_serializer(attendance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            content_data = {
+                'provided_by': "SMS API services",
+                'success': True,
+                'status': 200,
+                'data': serializer.data,
+            }
+            return Response(content_data, status=status.HTTP_200_OK)
+        else:
+            content_data = {
+                'provided_by': "SMS API services",
+                'success': False,
+                'status': 400,
+                'error': serializer.errors,
             }
             return Response(content_data, status=status.HTTP_400_BAD_REQUEST)
