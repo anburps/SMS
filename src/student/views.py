7ec53c802096b9480abe7650d9a8eddd8b30646f
anburps <anbalaganrps@gmail.com>
2024-10-30 20:40:19 +0530

    attenance data delete function added

--- src/student/views.py
+++ src/student/views.py
@@ -789,7 +789,7 @@ class AssignmentListView(generic.ListAPIView):
             return Response(content_data, status=status.HTTP_400_BAD_REQUEST)
 
 
-class AttenanceDetialsView(generics.RetrieveAPIView):
+class AttenanceDetialsView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = StudentSerializers.AttendanceSerializer
     queryset = Attendance.objects.all()
     authentication_classes = [BasicAuthentication, TokenAuthentication]
@@ -815,3 +815,13 @@ class AttenanceDetialsView(generics.RetrieveAPIView):
                 'error': serializer.errors,
             }
             return Response(content_data, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, *args, **kwargs):
+        attendance=self.get_object()
+        attendance.delete()
+        content_data = {
+            'provided_by': "SMS API services",
+            'success': True,
+            'status': 204,
+            'message': "Attendance successfully deleted."
+        }
+        return Response(content_data, status=status.HTTP_204_NO_CONTENT)
