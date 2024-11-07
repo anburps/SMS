309ab027b0ac505c986eb5b1155351990d300b93
anburps <anbalaganrps@gmail.com>
2024-11-07 21:07:57 +0530

    enrollement detail url

--- src/student/urls.py
+++ src/student/urls.py
@@ -7,9 +7,11 @@ urlpatterns = [
     path('student/detail/<int:id>/',StudentDetailView.as_view(),name='student-detail'),
     path('course/create/', CourseCreateView.as_view(), name='course-create'),
     path('course/list', CourseListView.as_view(), name='course-list'),
-    path('course/detail/', CourseDetailView.as_view(), name='course-create'),
+    path('course/detail/', CourseDetailView.as_view(), name='course-detail'),
     path('enrollment/create/', EnrollmentCreateView.as_view(), name='enrollment-create'),
     path('enrollment/list/',EnrollmentListView.as_view(),name='enrollment_list'),
+    path('enrollment/detail/', EnrollmentDetailView.as_view(), name='Enrollment-detail'),
+
     path('grade/create/', GradeCreateView.as_view(), name='grade-create'),
     path('attendance/create/', AttendanceCreateView.as_view(), name='attendance-create'),
     path('')
